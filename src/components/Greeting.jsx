6dcb429b4f3e2aec6215cac39c9c6fd145451960
Crista-Desuraune <cdesuraunecocu@normandiewebschool.fr>
2025-03-12 16:55:49 +0100

    ajout de tailwind et remplacement du css par tailwind

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,8 +9,8 @@ export default function Greeting({messages}) {
 
     return (
     <div>
-        <h3>{greeting} ! Merci de votre visite !</h3>
-        <button onClick={() => setGreeting(randomMessage())}>
+        <h3 class="block font-bold font-xl mt-4 mb-4 ml-0 mr-0 ">{greeting} ! Merci de votre visite !</h3>
+        <button class="block border rounded-lg text-sm py-1 px-6 hover:bg-blue hover:text-white" onClick={() => setGreeting(randomMessage())}>
         Nouvelle salutation
         </button>
     </div>
